3e2cfbb157ad6044bb2377ffdfe83411577fbead
andre-kiyuna <dehljkdl@gmail.com>
2026-04-06 19:33:03 -0300

    Add files via upload

--- Desenvolvimento de Software/Aula 9/Atividade2/src/Amigo.java
+++ Desenvolvimento de Software/Aula 9/Atividade2/src/Amigo.java
@@ -1,3 +1,20 @@
-public class Amigo {
-    
+public class Amigo extends Pessoa {
+    private String diaDoAniversario;
+
+    public Amigo(){
+        super();
+    }
+
+    public Amigo(String nome, char sexo, int idade, String diaDoAniversario){
+        super(nome, sexo, idade);
+        this.diaDoAniversario = diaDoAniversario;
+    }
+
+    public String getDiaDoAniversario(){
+        return diaDoAniversario;
+    }
+
+    public void setDiaDoAniversario(String diaDoAniversario){
+        this.diaDoAniversario = diaDoAniversario;
+    }
 }
